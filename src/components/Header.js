5257d96bb6f8f21b3c8d6dfdad0dbc75c60d159e
wahula4 <mawahula4@gmail.com>
2017-11-09 21:14:28 -0700

    styling indecision app

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Header = (props) => (
             // a subtitle will only be rendered if there is a subtitle
-            <div>
-                <h1>{props.title}</h1>
-                {props.subtitle && <h2>{props.subtitle}</h2>}
+            <div className="header">
+                <div className="container">
+                    <h1 className="header__title">{props.title}</h1>
+                    {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
+                </div>
             </div>
         );
 // default prop values
